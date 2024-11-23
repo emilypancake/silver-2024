787cc4e847e3ab06b9dbfeec3f608c4d21bf118b
Emily Pan <pancakerula@gmail.com>
2024-11-23 01:28:46 -0800

    adding BasicAuto commands

--- src/main/java/frc/robot/commands/BasicAuto.java
+++ src/main/java/frc/robot/commands/BasicAuto.java
@@ -7,9 +7,17 @@ import frc.robot.subsystems.Drivetrain;
 public class BasicAuto extends SequentialCommandGroup {
     public BasicAuto(Drivetrain drivetrain, Arm arm){
         addCommands( // this is a call to a function
-        // drive forward at 0.25 speed for 1.25
-            new AutoDrive(drivetrain, 0.25, 0.25).withTimeout(1.5) //time takes pause then wait deploy
-            new AutoDrive(drivetarin, 0.15, )
+        // drive forward at 0.25 speed for x sec
+        // turn if negative sped for x seconds
+        // 15 sec auto
+
+        //wait good for  turn = new WaitCommand(seconds)
+        //lift arm 
+            new AutoDrive(drivetrain, 0.25, 0.25).withTimeout(1.5), //time takes pause then wait deploy
+            new AutoDrive(drivetrain, 0.15, -0.15).withTimeout(0.75),
+            new LiftArm(arm, 0.4).withTimeout(0.5)
         );
+
+        
     }
 }
